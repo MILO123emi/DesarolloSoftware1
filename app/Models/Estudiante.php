f90cf11b5803567d729850060fb859fe2b491682
Diego Ayala <milo@192.168.1.9>
2026-01-29 14:40:57 -0600

    testeo

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Profile;
 class Estudiante extends Model
 {
     /** @use HasFactory<\Database\Factories\EstudianteFactory> */
     use HasFactory;
     protected $fillable = ["nombre", "facultad", "telefono", "correo"];
 
+    public function profile(){
+        return $this-> hasOne(Profile::class);
+    }
 }
